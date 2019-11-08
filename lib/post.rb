0f69520b6a005a0591d674634950d111e5f6f4a1
Wendy Harris <wendyv510@hotmail.com>
2019-11-08 19:17:50 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,22 +1,9 @@
-class Author 
-  
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-  end 
-  
-end 
+
 
 
 class Post 
   
-  attr_accessor :title, :author 
-  
-  def initialize(title, author)
-    @title = title
-    @author = author
-  end 
+  attr_accessor :title, :author
   
 end 
 
